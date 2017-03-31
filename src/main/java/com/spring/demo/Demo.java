11db641341f6942d220f44d36eda194a1a67e222
ramreddysrinivas <ramreddysrinivas532@gmail.com>
2017-03-31 10:37:20 +0530

    code addedfor demo, readme.md

--- src/main/java/com/spring/demo/Demo.java
+++ src/main/java/com/spring/demo/Demo.java
@@ -1,15 +1,48 @@
 package com.spring.demo;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 import com.spring.Address;
 import com.spring.Customer;
 import com.spring.Temp;
 
 public class Demo {
+	
 	public static void main(String[] args) {
+		Demo demo = new Demo();
+		//demo.usingApplicationContext();
+		demo.usingBeanFactory();
+	}
+	private void usingBeanFactory() {
+		Resource resource=null;
+		//resource=new ClassPathResource("spring-beans.xml");
+		
+		resource=new FileSystemResource("D:/core java/Spring2.5DIExample/src/main/resources/spring-beans.xml");
+		
+		BeanFactory factory = new XmlBeanFactory(resource);
+		
+		Customer customer = (Customer) factory.getBean("customer");
+		Address address=(Address) factory.getBean("addr");
+		
+		System.out.println("printing values declred in spring-beans.xml");
+		System.out.println(customer.getcId() + " , " + customer.getcName() + " , " + customer.getAddress() );
+		
+		customer.setcId(1234);
+		customer.setcName("name2");
+		customer.setAddress(address);
+		
+		System.out.println("printing overriden values.");
+		System.out.println(customer.getcId() + " , " + customer.getcName() + " , " + customer.getAddress() );
+	}
+	private void usingApplicationContext(){
+
 		ApplicationContext context =null;
 		
 		//loading xml using ClassPathXmlApplicationContext.
@@ -34,6 +67,7 @@ public class Demo {
 		
 		System.out.println("printing overriden values.");
 		System.out.println(customer.getcId() + " , " + customer.getcName() + " , " + customer.getAddress() );
+	
 	}
 
 }
